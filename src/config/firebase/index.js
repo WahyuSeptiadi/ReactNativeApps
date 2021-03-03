cc1f86d9ed7cc6dd866e105f978bbe943ee6c1a9
WahyuSeptiadi <wahyusptd@gmail.com>
2021-03-03 20:54:08 +0700

    setup firebase

---
+++ src/config/firebase/index.js
@@ -0,0 +1,14 @@
+import Firebase from 'firebase';
+
+Firebase.initializeApp({
+  apiKey: 'AIzaSyA9NTepNTXqSUuvwYYZ0ClbG6X_vj-7tKQ',
+  authDomain: 'note-react-native-5a6e9.firebaseapp.com',
+  projectId: 'note-react-native-5a6e9',
+  storageBucket: 'note-react-native-5a6e9.appspot.com',
+  messagingSenderId: '531135604774',
+  appId: '1:531135604774:web:72a9051fbee14c1f825ee7',
+});
+
+const firebase = Firebase;
+
+export default firebase;
