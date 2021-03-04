12234f5cc1f8ec1756ff807abbfa34ff994b22af
WahyuSeptiadi <arstuw@gmail.com>
2021-03-04 18:03:34 +0700

    check get data from database in console.log

--- src/pages/NoteList/index.js
+++ src/pages/NoteList/index.js
@@ -2,9 +2,37 @@ import React, {Component} from 'react';
 import {StyleSheet, View, TouchableOpacity} from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import FIREBASE from '../../config/Firebase';
 
 export default class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: {},
+      notesKey: [],
+    };
+  }
+
+  componentDidMount() {
+    FIREBASE.database()
+      .ref('Notes')
+      .once('value', (querySnapshot) => {
+        // * Cek querySnapshot datanya ada gak ?
+        let data = querySnapshot.val() ? querySnapshot.val() : {};
+        let noteItem = {...data};
+
+        this.setState({
+          notes: noteItem,
+          notesKey: Object.keys(noteItem),
+        });
+      });
+  }
+
   render() {
+    console.log('Notes :', this.state.notes);
+    console.log('Notes Key :', this.state.notesKey);
+
     return (
       <View style={styles.pages}>
         <View style={styles.wrapperButton}>
